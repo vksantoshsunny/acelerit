274f9ed980b1b5d19d23007f9fc10b1ca98d437c
vksantoshsunny <vksantoshsunny@gmail.com>
2019-05-11 14:15:58 +1200

    added colour

--- acelerit/src/App.js
+++ acelerit/src/App.js
@@ -2,18 +2,38 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: pink,
+    error: red,
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+});
 
 function App() {
   return (
-    <AppBar position="static" color="default">
+    <MuiThemeProvider theme={theme}>
+    <AppBar position="static" color="primary">
         <Toolbar>
           <Typography variant="h6" color="inherit">
-            Sample
+            Acelerit
           </Typography>
         </Toolbar>
       </AppBar>
+      </MuiThemeProvider>
   );
 }
 
